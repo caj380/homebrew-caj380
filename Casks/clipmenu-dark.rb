c909e9b467297544f1595eccbba5d289f861c9d8
Chase Johnson <cjohnson@cjohnson-mbp.attlocal.net>
2021-09-03 12:35:01 -0400

    Add ClipMenu and ClipMenu-Dark

---
+++ Casks/clipmenu-dark.rb
@@ -0,0 +1,18 @@
+cask "clipmenu-dark" do
+  version "0.4.4a12"
+
+  url "https://raw.githubusercontent.com/caj380/homebrew-caj380/master/Casks/ClipMenu-Dark_{version}.zip",
+  name "Clipmenu (Dark Mode edition)"
+  desc "Clipboard extension app"
+  homepage "http://www.clipmenu.com/"
+
+  depends_on macos: ">= :yosemite"
+
+  app "ClipMenu.app"
+
+  uninstall quit: "com.naotaka.ClipMenu"
+
+  zap trash: [
+    "~/Library/Application Support/ClipMenu",
+  ]
+end
---
+++ Casks/clipmenu.rb
@@ -0,0 +1,18 @@
+cask "clipmenu" do
+  version "0.4.4a12"
+
+  url "https://raw.githubusercontent.com/caj380/homebrew-caj380/master/Casks/ClipMenu_{version}.zip",
+  name "Clipmenu"
+  desc "Clipboard extension app"
+  homepage "http://www.clipmenu.com/"
+
+  depends_on macos: ">= :yosemite"
+
+  app "ClipMenu.app"
+
+  uninstall quit: "com.naotaka.ClipMenu"
+
+  zap trash: [
+    "~/Library/Application Support/ClipMenu",
+  ]
+end
